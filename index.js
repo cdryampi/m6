ab384a2cd906d9b3fd3f45a011134faf2213440f
yampi <cdryampi@gmail.com>
2019-09-20 17:06:51 +0200

    JQ y css

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'src')))
 
 // listen the server
-const server = app.listen(app.get('port'), () => {
-    console.log('Listening on port', app.get('port'))
+const server = app.listen(3000, '0.0.0.0', () => {
+    console.log('Estic escoltant al port ', app.get('port'))
 })
--- src/public/js/core.js
+++ src/public/js/core.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+    $('.btn-sm').css('margin', '5px')
     console.log("It works")
+
+    $('.btn-danger').click(function(e) {
+        alert("El color rojo es del peligro")
+    });
+    $('.btn-success').click(function(e) {
+        alert("El color verde es de la vida")
+    });
+    $('.btn-info').click(function(e) {
+        alert("El color celeste es para informar")
+    });
+    $('.btn-primary').click(function(e) {
+        alert("El color azul es el generico")
+    });
+
+    $("#envia").click(function(e) {
+        var content = $("#text").val()
+        var color = $("#color").val()
+        console.log(content + "-" + color)
+
+        $("#botones").append(content).css("color", color)
+
+
+
+
+
+    });
 })
